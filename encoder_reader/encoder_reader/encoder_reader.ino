3651182ec3ada24bc1cc41b0a1fce89c5785449d
Turgay Senlet <turgaysenlet@gmail.com>
2017-04-08 12:18:25 -0500

    Move encoder reader

--- encoder_reader/encoder_reader/encoder_reader.ino
+++ encoder_reader/encoder_reader/encoder_reader.ino
@@ -1,6 +1,6 @@
 int val;
-int encoder0PinA = 8;
-int encoder0PinB = 9;
+int encoder0PinA = 10;
+int encoder0PinB = 11;
 int encoder0Pos = 0;
 int encoder0PinALast = LOW;
 int n = LOW;
@@ -21,9 +21,8 @@ void loop() {
       encoder0Pos++;
     }
     Serial.println(encoder0Pos);
-    //Serial.print("/");
-  }
-  digitalWrite(LED_BUILTIN, n);
+    digitalWrite(LED_BUILTIN, n);
+  }  
   encoder0PinALast = n;
   //delay(10);
 }
