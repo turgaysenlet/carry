1068809fd6537ba99d078661784485f2ed4177f2
Turgay Senlet <turgaysenlet@gmail.com>
2018-02-22 00:01:21 -0600

    Synchronized depth image and camera info publishing

--- obstable_detector/src/obstable_detector_node_backup2.cpp
+++ obstable_detector/src/obstable_detector_node_backup2.cpp
@@ -1,31 +1,39 @@
+#include <costmap_2d/costmap_2d_ros.h>
+#include <cv_bridge/cv_bridge.h>
+#include <errno.h> // Error number definitions
+#include <fcntl.h> // File control definitions
+#include <image_transport/image_transport.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/synchronizer.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <ros/console.h>
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
-#include <opencv2/opencv.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <stereo_msgs/DisparityImage.h>
-#include <image_transport/image_transport.h>
 #include <sensor_msgs/image_encodings.h>
-#include <cv_bridge/cv_bridge.h>
 #include <stdio.h> // standard input / output functions
+#include <stereo_msgs/DisparityImage.h>
 #include <string.h> // string function definitions
-#include <unistd.h> // UNIX standard function definitions
-#include <fcntl.h> // File control definitions
-#include <errno.h> // Error number definitions
 #include <termios.h> // POSIX terminal control definitions
-#include <time.h>   // time calls
-#include <ros/console.h>
 #include <tf/transform_listener.h>
-#include <costmap_2d/costmap_2d_ros.h>
+#include <time.h>   // time calls
+#include <unistd.h> // UNIX standard function definitions
 
 using namespace std;
 using namespace ros::console::levels;
 using namespace sensor_msgs;
+using namespace message_filters;
 using namespace stereo_msgs;
 namespace enc = sensor_msgs::image_encodings;
 
 const float HEIGHT_CENTER = 0.5f;
 const float PLANE_RATIO = 200.0f;
+
+// Forward decleration
+class ObstableDetectorCls; 
+ObstableDetectorCls* obstable_detector = NULL;
 
 void depthToCV8UC1(const cv::Mat& float_img, cv::Mat& mono8_img){
 	//Process images
@@ -44,54 +52,80 @@ void depthToCV8UC1(const cv::Mat& float_img, cv::Mat& mono8_img){
 class ObstableDetectorCls
 {
 public:
-	ObstableDetectorCls();	
+	ObstableDetectorCls();
+	void disparityCallback(const stereo_msgs::DisparityImage::ConstPtr& disp, 
+		const sensor_msgs::CameraInfo::ConstPtr& info);
 private:
-	std::string frame;
-	CameraInfo c1;
 	ros::NodeHandle nh_;
-	ros::Subscriber camera_info_sub_;
+	image_transport::ImageTransport it_;
 	ros::Publisher camera_info_pub_;
-	image_transport::ImageTransport it_;	
 	image_transport::Publisher top_view_pub_;
 	image_transport::Publisher top_view_rotated_pub_;
 	image_transport::Publisher depth_pub_;
-	ros::Subscriber disp_image_sub_;
-	void disparityCallback(const stereo_msgs::DisparityImage::ConstPtr& disp_image_msg);
-	void cameraInfoCallback(const sensor_msgs::CameraInfo::ConstPtr& info);
+	image_transport::Publisher depth_pub2_;
+	std::string frame;
+	//ros::Subscriber disp_image_sub_;
+	//ros::Subscriber camera_info_sub_;
 	
+	//void disparityCallback(const stereo_msgs::DisparityImage::ConstPtr& disp_image_msg);		
 	int Width;
 	int Height;
 };
 
+// Global function, just to get around error 'ISO C++ forbids taking the address of 
+// an unqualified or parenthesized non-static member function to form a pointer to 
+// member function. Say ‘&ObstableDetectorCls::disparityCallback’ [-fpermissive]'
+void disparityCallbackBase(const stereo_msgs::DisparityImage::ConstPtr& disp, 
+	const sensor_msgs::CameraInfo::ConstPtr& info)
+{
+	ROS_INFO("Hello, again.");
+	if (obstable_detector != NULL) 
+	{
+		obstable_detector->disparityCallback(disp, info);
+	}
+}
+
 ObstableDetectorCls::ObstableDetectorCls() : it_(nh_)
 {
+	ROS_INFO("Constructor start...");
 	frame = "camera";
-	camera_info_sub_ = nh_.subscribe("/stereo/left/camera_info", 1, &ObstableDetectorCls::cameraInfoCallback, this);
-	disp_image_sub_ = nh_.subscribe("/stereo/disparity", 1, &ObstableDetectorCls::disparityCallback, this);	
+	message_filters::Subscriber<DisparityImage> disp_image_sub(nh_, "/stereo/disparity", 10);
+	disp_image_sub.unsubscribe();
+
+	message_filters::Subscriber<CameraInfo> camera_info_sub(nh_, "/stereo/left/camera_info", 10);
+	camera_info_sub.unsubscribe();
+
+	typedef sync_policies::ApproximateTime<DisparityImage, CameraInfo> MySyncPolicy;
+    Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), disp_image_sub, camera_info_sub);
+   	sync.setAgePenalty(1000);
+    sync.registerCallback(boost::bind(&disparityCallbackBase, _1, _2));
+  	
+  	camera_info_pub_ = nh_.advertise<CameraInfo>("stereo/camera_info", 1);
+
 	top_view_pub_ = it_.advertise("stereo/top_view", 1);
 	top_view_rotated_pub_ = it_.advertise("stereo/top_view_rotated", 1);
 	depth_pub_ = it_.advertise("stereo/depth", 1);
-	camera_info_pub_ = nh_.advertise<CameraInfo>("stereo/camera_info", 1);
+	depth_pub2_ = it_.advertise("stereo/depth2", 1);
+	ROS_INFO("Constructor done.");
 }
 
-void ObstableDetectorCls::cameraInfoCallback(const sensor_msgs::CameraInfo::ConstPtr& info)
-{
-	ROS_INFO("CameraInfo received.");		
-	c1 = *info;
-}
-
+	//void ObstableDetectorCls::disparityCallback(
+		//const stereo_msgs::DisparityImage& disp_image_msg)
+		//const stereo_msgs::DisparityImage::ConstPtr& disp_image_msg)
 void ObstableDetectorCls::disparityCallback(
-	//const stereo_msgs::DisparityImage& disp_image_msg)
-	const stereo_msgs::DisparityImage::ConstPtr& disp_image_msg)
+	const stereo_msgs::DisparityImage::ConstPtr& disp_image_msg, 
+	const sensor_msgs::CameraInfo::ConstPtr& info)
 {
-	ROS_INFO("Disparity image received.");		
+	ROS_INFO("Disparity image received.");
 	//cv_bridge::CvImageConstPtr cv_ptr_disp = cv_bridge::toCvShare(disp_image_msg->image, disp_image_msg, sensor_msgs::image_encodings::TYPE_32FC1);
 	cv_bridge::CvImageConstPtr cv_ptr_disp = cv_bridge::toCvCopy(disp_image_msg->image, sensor_msgs::image_encodings::TYPE_32FC1);
 	const cv::Mat disp_image = cv_ptr_disp->image;	
 	ROS_INFO("Disparity image converted.");	
 	ROS_INFO("Image size %dx%d", disp_image.cols, disp_image.rows);
 	// Top view grayscale image
-	cv::Mat depth_image(disp_image.rows, disp_image.cols, CV_32FC1, cv::Scalar(0));	
+	cv::Mat depth_image(disp_image.rows, disp_image.cols, CV_32FC1, cv::Scalar(0));
+	// Top view grayscale image, only part that will be converted to top view (a middle slice)
+	cv::Mat depth_image2(disp_image.rows, disp_image.cols, CV_32FC1, cv::Scalar(0));
 	cv::Mat top_view_image(64, 64, CV_32FC1, cv::Scalar(0));
 	float xk = (float)disp_image.cols / (float)64;
 	float yk = (float)disp_image.rows / (float)64;
@@ -104,20 +138,21 @@ void ObstableDetectorCls::disparityCallback(
 		float jj = (j - depth_image.rows / 2.0f) / (depth_image.rows / 2.0f);
 		for (int i = 0; i < depth_image.cols; i++)
 		{
+			float d = 1.0f / disp_image.at<float>(j, i);
+			depth_image.at<float>(j, i) = d;
 			if (j < depth_image.rows * HEIGHT_CENTER)
 			{
-				depth_image.at<float>(j, i) = 0;
+				depth_image2.at<float>(j, i) = 0;
 			}
 			else 
 			{								
 				if (disp_image.at<float>(j, i) / jj > PLANE_RATIO)
 				{
-					float d = 1.0f / disp_image.at<float>(j, i);
-					depth_image.at<float>(j, i) = d;
+					depth_image2.at<float>(j, i) = d;
 					int y = (int)(d*dk);
 					if (y < 0) y = 0;
 					if (y > top_view_image.cols - 1) y = top_view_image.cols - 1;
-					int x = (int)((depth_image.cols / 2 - i) / xk + top_view_image.cols / 2);
+					int x = (int)((depth_image2.cols / 2 - i) / xk + top_view_image.cols / 2);
 					if (x < 0) x = 0;
 					if (x > top_view_image.cols - 1) x = top_view_image.cols - 1;
 					
@@ -125,13 +160,12 @@ void ObstableDetectorCls::disparityCallback(
 				} 
 				else 
 				{
-					depth_image.at<float>(j, i) = 0;
+					depth_image2.at<float>(j, i) = 0;
 				}
 			}
 		}
 	}
 	
-
 	//cvtColor(disp_image, rgb_image, CV_GRAY2BGR );
 	
 	//ROS_INFO("Disparity image color converted.");		
@@ -142,6 +176,7 @@ void ObstableDetectorCls::disparityCallback(
 	//Top view image
 	cv::Mat top_mono_image;
 	cv::Mat depth_mono_image;
+	cv::Mat depth_mono_image2;
 	//cvtColor(top_view_image, top_rgb_image, CV_GRAY2BGR );
 	depthToCV8UC1(top_view_image, top_mono_image);
 	cv_bridge::CvImage cv_top_view_ptr(header, enc::MONO8, top_mono_image);
@@ -158,15 +193,30 @@ void ObstableDetectorCls::disparityCallback(
 	depthToCV8UC1(depth_image, depth_mono_image);
 	cv_bridge::CvImage cv_depth_ptr(header, enc::MONO8, depth_mono_image);	
 	depth_pub_.publish(cv_depth_ptr.toImageMsg());
+	
+	depthToCV8UC1(depth_image2, depth_mono_image2);
+	cv_bridge::CvImage cv_depth_ptr2(header, enc::MONO8, depth_mono_image2);	
+	depth_pub2_.publish(cv_depth_ptr2.toImageMsg());
+	
+	//CameraInfo c1 = *info;
+	//camera_info_pub_.publish(c1);
+	// CameraInfoPtr info_right(new CameraInfo(right_info_mgr.getCameraInfo()));
+
+ //  info_left->header.stamp = time;
+ 
+
+ //  left_info_pub.publish(info_left);
+ //  right_info_pub.publish(info_right);
 	//disp_image.deallocate();
 	//ROS_INFO("Depth image published.");
-	c1.header = header;
-	camera_info_pub_.publish(c1);
 }
 
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "ObstableDetector");
-	ObstableDetectorCls obstable_detector;
+	ROS_INFO("Hello.");
+	obstable_detector = new ObstableDetectorCls();
 	ros::spin();
+	delete obstable_detector;
 }
+
