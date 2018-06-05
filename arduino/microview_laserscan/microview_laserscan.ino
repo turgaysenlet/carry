7a2eda12a7b3fb02e1f86d75fe258d3887000966
Turgay Senlet <turgaysenlet@gmail.com>
2018-06-05 00:59:54 -0500

    Arduino microview laserscan display

--- arduino/microview_laserscan/microview_laserscan.ino
+++ arduino/microview_laserscan/microview_laserscan.ino
@@ -17,10 +17,24 @@ ros::NodeHandle  nh;
 MicroViewWidget *widget1;
 
 void scan_cb( const std_msgs::String& cmd_msg) {
-  
-  a++;
-  a %= 100;
-  widget1->setValue(a++);
+  // a++;
+  // a %= 100;
+  uView.clear(PAGE);
+  //widget1->setValue(a++);
+  //uView.setCursor(0,50);
+  //uView.print(cmd_msg.data);
+  float p = 3.0f;
+  float t = 45.0f;
+  float x = (float)SCREEN_WIDTH/20.0f;
+  uView.line(SCREEN_WIDTH/2,SCREEN_HEIGHT,SCREEN_WIDTH/2-3,SCREEN_HEIGHT-3);
+  uView.line(SCREEN_WIDTH/2,SCREEN_HEIGHT,SCREEN_WIDTH/2+3,SCREEN_HEIGHT-3);
+  uView.line(0,SCREEN_HEIGHT-3,SCREEN_WIDTH,SCREEN_HEIGHT-3);
+  for (int i = 2; i < 40; i+=2) {
+    int ii = i/2;
+    float y0 = (float)(cmd_msg.data[i-2]- 48) * 10.0 + (float)(cmd_msg.data[i-1]- 48);
+    float y1 = (float)(cmd_msg.data[i]- 48) * 10.0 + (float)(cmd_msg.data[i+1]- 48);
+    uView.line(x*(ii-1),t-p*y0,x*ii,t-p*y1);
+  }
   uView.display();
 }
 
@@ -32,16 +46,11 @@ void setup() {
   // put your setup code here, to run once:
   uView.begin();
   uView.clear(PAGE);      // clear page
-  widget1 = new MicroViewSlider(0,0,0,100);   // declare widget0 as a Slider at x=0, y=0, min=0, max=100
+  //widget1 = new MicroViewSlider(0,0,0,100);   // declare widget0 as a Slider at x=0, y=0, min=0, max=100
   nh.subscribe(sub);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:  
-  //widget1->setValue(a);
-  //uView.display();
-  a %= 100;
-  //uView.line(10,10,100,100);
   delay(10);
   nh.spinOnce();
 }
---
+++ arduino/process_laser_for_microview.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+import math
+from std_msgs.msg import String
+from sensor_msgs.msg import LaserScan
+
+def callback(data):
+    # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.ranges)
+    i = 1
+    l = len(data.ranges)
+    s = ""
+    for x in data.ranges:
+        x = x-0.4
+        if i % 32 == 15:
+            if math.isnan(x):
+                x = 9
+            if x>=9:
+                x = 9
+            
+            xx = int(x*10.0)
+            xs = str(xx)
+            if xx < 10:
+                xs = "0" + xs
+            s = xs + s 
+            print([str(i),str(x),xs])
+        i = i + 1
+    print s
+    pub.publish(s)
+    
+def listener():
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber("scan", LaserScan, callback)
+    
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    pub = rospy.Publisher('scan_string', String, queue_size=10)
+    try:
+        listener()
+    except rospy.ROSInterruptException:
+        pass
